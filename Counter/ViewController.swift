6f371d9ed7abbac0af29e07c2a0822b6860b8ee1
AntonIshchenko1983 <anton-almaty@yandex.ru>
2024-07-16 19:25:50 +0500

    Правки по указаниям ревьювера

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController, UITextViewDelegate {
 
-    @IBOutlet weak var textViewZone: UITextView!
-    @IBOutlet weak var buttonMinusOutlet: UIButton!
-    @IBOutlet weak var buttonPlusOutlet: UIButton!
-    @IBOutlet weak var buttonZeroUotlet: UIButton!
-    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet private weak var textViewZone: UITextView!
+    @IBOutlet private weak var buttonMinusOutlet: UIButton!
+    @IBOutlet private weak var buttonPlusOutlet: UIButton!
+    @IBOutlet private weak var buttonZeroUotlet: UIButton!
+    @IBOutlet private weak var counterLabel: UILabel!
     
     private var counter:Int = 0
     
@@ -39,9 +39,10 @@ class ViewController: UIViewController, UITextViewDelegate {
         self.buttonPlusOutlet.layer.cornerRadius = 17
         self.buttonZeroUotlet.layer.cornerRadius = 17
         textViewZone.delegate = self
+        textViewZone.textContainerInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 0)
     }
 
-    @IBAction func buttonPlus() {
+    @IBAction private func onTapButtonPlus() {
         counter += 1
         counterLabel.text = "Значение счетчика: " + String(counter)
         let stringDate = GetDateAndTime()
@@ -49,7 +50,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         textViewDidChange(textViewZone)
     }
     
-    @IBAction func buttonMinus(_ sender: Any) {
+    @IBAction private func onTapButtonMinus(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             counterLabel.text = "Значение счетчика: " + String(counter)
@@ -63,7 +64,7 @@ class ViewController: UIViewController, UITextViewDelegate {
         }
     }
     
-    @IBAction func buttonZero() {
+    @IBAction private func onTapButtonZero() {
         counter = 0
         counterLabel.text = "Значение счетчика: " + String(counter)
         let stringDate = GetDateAndTime()
